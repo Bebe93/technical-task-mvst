f818f6a44ea1e398186f5c94052f5672dee0e5d6
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-05-18 17:57:16 +0200

    start

--- mvst-assignment/src/App.js
+++ mvst-assignment/src/App.js
@@ -1,6 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
+  getRepos = async () => {
+    try {
+      const reposList = axios.get("", {});
+      setRepos(reposList.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header"></header>
